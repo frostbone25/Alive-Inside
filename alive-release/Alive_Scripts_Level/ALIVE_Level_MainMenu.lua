841ebd1daf1bf7e0a8f88922b1838b4609df5e32
Violet <66211311+droyti@users.noreply.github.com>
2022-06-21 01:19:30 -0400

    implement vanilla UI

--- alive-release/Alive_Scripts_Level/ALIVE_Level_MainMenu.lua
+++ alive-release/Alive_Scripts_Level/ALIVE_Level_MainMenu.lua
@@ -1,3 +1,9 @@
+--Enable requisite ResourceSets
+ResourceSetEnable("ProjectSeason4");
+ResourceSetEnable("Project");
+ResourceSetEnable("Menu");
+ResourceSetEnable("WalkingDead404");
+
 require("ALIVE_Core_Math.lua");
 require("ALIVE_Core_Utilities.lua");
 require("ALIVE_Core_AgentExtensions_Properties.lua");
@@ -11,13 +17,22 @@ require("ALIVE_Development_Freecam.lua");
 require("ALIVE_Development_AgentBrowser.lua");
 require("ALIVE_Core_Project.lua");
 
---Enable requisite ResourceSets
-ResourceSetEnable("ProjectSeason4");
-ResourceSetEnable("WalkingDead404");
+--Project//Menu
+require("UI_ListButton.lua")
+require("UI_ListButtonLite.lua")
+require("UI_Legend.lua")
+require("UI_Popup.lua")
+require("Utilities.lua")
+require("MenuUtils.lua")
+require("RichPresence.lua")
+require("AspectRatio.lua")
+
+local bgMain = {isMenuMain = true}
 
 --Engine Scene Variables
 local kScript = "ALIVE_Level_MainMenu"
-local kScene = "adv_boardingSchoolDorm"
+local kScene = "ui_menuMain.scene" --adv_boardingSchoolDorm
+local keyArtScene = "adv_boardingSchoolDorm"
 local kSceneObj = kScene .. ".scene"
 
 --Cutscene Development Variables
@@ -48,24 +63,77 @@ ALIVE_MainMenu_PrepareCamera = function()
     local camPosition = Vector(15.05, 1, -4.32);
     local camRotation = Vector(0, 90, 0);
 
-    local menuCamera = AgentCreate("ALIVE_MainMenuCamera", camProp, camPosition, camRotation, kScene, false, false);
+    local menuCamera = AgentCreate("ALIVE_MainMenuCamera", camProp, camPosition, camRotation, keyArtScene, false, false);
     
-    ALIVE_AgentSetProperty("ALIVE_MainMenuCamera", "Clip Plane - Far", 2500, kScene);
-    ALIVE_AgentSetProperty("ALIVE_MainMenuCamera", "Clip Plane - Near", 0.05, kScene);
-    ALIVE_AgentSetProperty("ALIVE_MainMenuCamera", "Lens - Current Lens", nil, kScene);
-    ALIVE_RemovingAgentsWithPrefix(kScene, "cam_");
+    ALIVE_AgentSetProperty("ALIVE_MainMenuCamera", "Clip Plane - Far", 2500, keyArtScene);
+    ALIVE_AgentSetProperty("ALIVE_MainMenuCamera", "Clip Plane - Near", 0.05, keyArtScene);
+    ALIVE_AgentSetProperty("ALIVE_MainMenuCamera", "Lens - Current Lens", nil, keyArtScene);
+    ALIVE_RemovingAgentsWithPrefix(keyArtScene, "cam_");
 
     CameraPush("ALIVE_MainMenuCamera");
 end
 
 ALIVE_MainMenu_PrepareAgents = function()
     --Prepare all scene agents
+    local clemHat = AgentCreate("ALIVE_MainMenuClemHat", "obj_capClementine400.prop", Vector(17.12, 0.82, -4.32), Vector(-5, -65.7, 0), keyArtScene, false, false)
+end
+
+ALIVE_MainMenu_CreateAndPopulateMenu = function()
+    if not SceneIsActive(keyArtScene) then
+        MenuUtils_AddScene(keyArtScene)
+    end
+    SceneHide(keyArtScene, false)
+    SceneHide("ui_menuMain", false)
+
+    local menu = Menu_Create(ListMenu, "ui_menuMain", kScene)
+    menu.align = "left"
+    menu.background = bgMain
+
+    menu.Show = function(self, direction)
+        if direction and direction < 0 then
+            ChorePlay("ui_alphaGradient_show")
+        end
+        ;
+        (Menu.Show)(self)
+        RichPresence_Set("richPresence_mainMenu", false)
+    end
+
+    menu.Hide = function(self, direction)
+        ChorePlay("ui_alphaGradient_hide")
+        ;
+        (Menu.Hide)(self)
+    end
+
+    menu.Populate = function(self)
+         Menu_Add(ListButtonLite, "exit", "label_exitGame", "UI_Confirm( \"popup_quit_header\", \"popup_quit_message\", \"EngineQuit()\" )")
+        local legendWidget = Menu_Add(Legend)
+            legendWidget.Place = function(self)
+            self:AnchorToAgent(menu.agent, "left", "bottom")
+        end
+    end
     
-    local clemHat = AgentCreate("ALIVE_MainMenuClemHat", "obj_capClementine400.prop", Vector(17.12, 0.83, -4.32), Vector(10, -79.7, 0), kScene, false, false)
+    Menu_Show(menu);
+end
+
+ALIVE_MainMenu_PrepareMenu = function()
+    if Input_UseTouch() then
+        ClickText_Enable(true)
+    end
+    local prefs = GetPreferences()
+    if PropertyIsLocal(prefs, "Menu - User Gamma Setting") then
+        RenderSetIntensity(PropertyGet(prefs, "Menu - User Gamma Setting"))
+        PropertyRemove(prefs, "Menu - User Gamma Setting")
+        SavePrefs()
+    end
+    RenderForce_16_by_9_AspectRatio(true)
+    RenderDelay(1)
+    WaitForNextFrame()
+    ALIVE_MainMenu_CreateAndPopulateMenu();
 end
 
 ALIVE_Level_MainMenu = function()
 
+    ALIVE_MainMenu_PrepareMenu();
     ALIVE_MainMenu_PrepareAgents();
 
     if (ALIVE_Core_Project_IsDebugMode) and (EnableFreecamTools) then
